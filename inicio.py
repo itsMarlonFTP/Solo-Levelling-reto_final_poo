3fd95e0546dfd1e383ad555f6b8703687274d792
itsMarlonFTP <mdanielcarvallo@gmail.com>
2023-03-22 16:38:36 -0600

    Update inicio.py
    registro de stats de los personajes

--- inicio.py
+++ inicio.py
@@ -1,6 +1,42 @@
-class Usuarios:
-    def __init__(self) -> None:
-        pass
-class Enemigos:
-    def __init__(self) -> None:
-        pass
+class Stats:
+    def __init__(self,hp,mana,power,strength) -> None:
+        self.hp:int=hp
+        self.mana:int=mana
+        self.power:int=power
+        self.strength:int=strength
+    
+    def Warrior(self):
+        self.hp = 150
+        self.mana=60
+        self.power=0
+        self.strength=50
+        
+    def Hunter(self):
+        self.hp = 100
+        self.mana=60
+        self.power=0
+        self.strength=100
+        
+    def Mage(self):
+        self.hp = 125
+        self.mana=150
+        self.power=50
+        self.strength=15
+    
+    def Goblins(self):
+        self.hp = 150
+        self.mana=60
+        self.power=0
+        self.strength=50
+        
+    def Skinwalker(self):
+        self.hp = 150
+        self.mana=60
+        self.power=0
+        self.strength=50
+        
+    def Boss(self):
+        self.hp = 150
+        self.mana=60
+        self.power=0
+        self.strength=50
